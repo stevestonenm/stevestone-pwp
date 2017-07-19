ceb4ecbddebefe242d32a8be19e982914ca0e01e
Steve Stone <stoneflynm@gmail.com>
2017-07-18 20:49:43 -0600

    completed first 2 sections of persona

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -9,11 +9,15 @@
 		<p><strong>Audience:</strong>Hardwoeking people know the benefits of ethnobotany and those new to the arena.</p>
 		<p><strong>Goal:</strong>The goal is to increase sales through an online retail site<p>
 		<h1>Persona</h1>
-		<p><strong>Name:</strong><br>
-			<strong>Age:</strong> <br></p>
-		<p><strong>Profession:</strong></p>
-		<p><strong>Technology:</strong></p>
-		<p><strong>Attitudes and Behaviors:</strong></p>
+		<p><strong>Name:</strong>Barbara O'Malley<br>
+			<strong>Age:</strong>44<br></p>
+		<p><strong>Profession:</strong>Office Manager at a large retail chain</p>
+		<p><strong>Technology:</strong>Uses her desktop ar work day in and day out to keep up with the flow of all
+			necessary paperwork that is sent and received from the corporate office.  Most of her work experience is with
+			Microsoft Office Suite but she does use a few other programs to scan, edit, and send documents. Outside of work,
+			she is frequently on her iPhone7 looking and Facebook and Instagram, her age group is the largest age group on
+			Facebook. She also uses her phone to complete most of her online shopping.</p>
+		<p><strong>Attitudes and Behaviors:</strong>She has been a long time sufferer of</p>
 		<p><strong>Needs:</strong></p>
 		<p><strong>Goals:</strong></p>
 		<p><strong>User Story:</strong></p>
