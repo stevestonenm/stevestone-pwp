34a7f61b6919243847499c91c09e10e5a57c472b
Steve Stone <stoneflynm@gmail.com>
2017-07-18 20:26:43 -0600

    purpose, audience, goal section

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -4,9 +4,13 @@
 		<meta charset="utf-8">
 	</head>
 	<body>
+		<h1>Purpose, Audience, & Goal</h1>
+		<p><strong>Purpose:</strong>My personal website project will establish my presence as a go to for plants and herbs.</p>
+		<p><strong>Audience:</strong>Hardwoeking people know the benefits of ethnobotany and those new to the arena.</p>
+		<p><strong>Goal:</strong>The goal is to increase sales through an online retail site<p>
 		<h1>Persona</h1>
 		<p><strong>Name:</strong><br>
-			<strong>Age:</strong><br></p>
+			<strong>Age:</strong> <br></p>
 		<p><strong>Profession:</strong></p>
 		<p><strong>Technology:</strong></p>
 		<p><strong>Attitudes and Behaviors:</strong></p>
