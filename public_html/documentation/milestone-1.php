a98bedc0428f844ffaf04bfee306b23349f04457
Steve Stone <stoneflynm@gmail.com>
2017-07-18 19:40:35 -0600

    updated html

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -4,6 +4,17 @@
 		<meta charset="utf-8">
 	</head>
 	<body>
-
+		<h1>Persona</h1>
+		<p><strong>Name:</strong><br>
+			<strong>Age:</strong><br></p>
+		<p><strong>Profession:</strong></p>
+		<p><strong>Technology:</strong></p>
+		<p><strong>Attitudes and Behaviors:</strong></p>
+		<p><strong>Needs:</strong></p>
+		<p><strong>Goals:</strong></p>
+		<p><strong>User Story:</strong></p>
+		<h1>Use Case</h1>
+		<p></p>
+		<p>.</p>
 	</body>
 </html>
