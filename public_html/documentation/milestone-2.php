10235e3b7628217570f30705d333e5783f8a6bb2
Steve Stone <stoneflynm@gmail.com>
2017-08-01 20:18:12 -0600

    added mobile wireframe

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -4,7 +4,30 @@
 		<meta charset="utf-8">
 		<title>Milestone-2</title>
 	</head>
+
 	<body>
+		<h1>Content Strategy</h1>
+		<p>
+			My personal website will be one page.  There will be four different sections. On a desk top monitor each section will occupy an equal quadrant of the screen. On a mobile device the quadrants will scale into stacked tiles on the screen. The sections are as follows:
+		</p>
+		<p>
+			<strong>What Are Ethnobotanicals:</strong>This section will consist of a through definition of what ethnobotany. It will explain the root of the word, traditional use, and modern use.
+		</p>
+		<p>
+			<strong>Popular Ethnobotanicals:</strong> There will be numerous plants and herbs listed in this section. Each will have what part of the world they are generally from and the traditional use.
+		</p>
+		<p>
+			<strong>Resources</strong> In the resources section, there will be different links to informative site here, nothing will link to any vendors.
+		</p>
+		<p>
+			<strong>Contact Us</strong> Contained in this section will be a contact form. There will also be a short paragraph about us. When the ecommerce site launches this section will also contain a link to my store.
+		</p>
+		<h2>Wireframes</h2>
+		
+		<h3>Mobile</h3>
+		<img src="images/mobile.png" alt="mobile wireframes">
+
+		<h3>Desktop</h3>
 
 	</body>
 </html>
