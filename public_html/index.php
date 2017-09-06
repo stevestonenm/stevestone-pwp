d4bce805374d3c3898ef9d4832f23384928c9d81
Steve Stone <stoneflynm@gmail.com>
2017-09-05 22:09:16 -0600

    added htaccess

--- public_html/index.php
+++ public_html/index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html xmlns="http://www.w3.org/1999/html">
+<html>
 	<head>
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -12,8 +12,6 @@
 		<!-- Optional theme -->
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
 
-		<!-- Font Awesome -->
-		<link type="text/css" href="//maxcdn.bootstrapcdn.com/font-awesome/4.6.2/css/font-awesome.min.css" rel="stylesheet" />
 
 		<!--custom css goes here-->
 		<link rel="stylesheet" href="css/stylesheet.css" type="text/css">
@@ -42,8 +40,6 @@
 
 		<!-----------Google recaptcha---------------->
 		<script src='https://www.google.com/recaptcha/api.js'></script>
-
-
 
 		<!-- Latest compiled and minified JavaScript -->
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
@@ -92,13 +88,6 @@
 		<section>
 			<div id="length" class="pad-section">
 				<div class="container-fluid">
-<!--					<div class="row">-->
-<!--						<div class="col-xs-12">-->
-<!--							<div class="text-vcenter">-->
-<!--								<h2></h2>-->
-<!--							</div>-->
-<!--						</div>-->
-<!--					</div>-->
 				</div>
 			</div>
 		</section>
@@ -170,17 +159,17 @@
 									<div class="media-body">
 										<h4 class="media-heading lead">San Pedro Cactus</h4>
 										Used for thousands of years in Mesoamerica, this hallucinogenic cactus has been referred to as a miracle cure for illnesses that Western Medicine has no cures for. The tea is usually drank in a ceremony led by a “curandero” or shaman.
-									</div>
+								</div>
 								</div>
 							</div>
-						</div>
+						</div> <!--closes panel-default-->
 					</div>
-				</div>
-			</div>
+				</div> <!--closes container-fluid-->
+			</div> <!--closes #popular-->
 		</section>
 		<!-- fourth section -->
 		<section>
-			<div id="resources" class="pad-section">
+			<div id="resources">
 				<div class="container-fluid">
 					<div class="row">
 						<div class="col-md-6 lead">
@@ -200,7 +189,7 @@
 							<a href="http://www.maya-ethnobotany.org/">
 								<span class="glyphicon glyphicon-leaf" aria-hidden="true">Maya Ethnobotany</span><br>
 							</a>
-						</div>
+						</div> <!--closes col-md-6-->
 
 						<!--contact form-->
 						<div class="col-md-6 lead">
@@ -255,17 +244,10 @@
 							<!--form error/success message area-->
 							<div id="output-area"></div>
 
-						</div>
+						</div> <!--closes contact form col-md-6-->
 					</div>
-<!--					<div class="row">-->
-<!--						<div class="col-md-12">-->
-<!--							<p class="disclaimer">-->
-<!--								Disclaimer: The information contained on this site is for informational purposes. It is not meant as a substitute for a medical professional. If you suspect you have a disease or health-related condition of any kind, you should contact your health care professional immediately. Please read all product packaging carefully and consult with a healthcare professional before starting any diet, exercise, supplementation or medication program. These statements have not been evaluated by the Food and Drug Administration and are not intended to diagnose, treat, cure, or prevent disease.-->
-<!--							</p>-->
-<!--						</div>-->
-					</div>
-				</div>
-			</div>
+				</div> <!--closes contaner-fluid-->
+			</div> <!--closes #resources-->
 		</section>
 
 		<footer class="footer">
